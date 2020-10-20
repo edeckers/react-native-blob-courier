40996de54acf23698f4266a9bfbea640a870a4de
Ely Deckers <e.deckers@gmail.com>
2020-10-21 00:46:54 +0200

    improvement: split upload, download into buttons

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,5 +1,13 @@
 import * as React from 'react';
-import { StyleSheet, View, Text, PermissionsAndroid } from 'react-native';
+import * as Progress from 'react-native-progress';
+
+import {
+  StyleSheet,
+  View,
+  Text,
+  PermissionsAndroid,
+  Button,
+} from 'react-native';
 import BlobCourier from 'react-native-blob-courier';
 import {
   AndroidBlobRequest,
@@ -12,6 +20,9 @@ import {
 
 export const App = () => {
   const [downloadResult, setDownloadResult] = React.useState<BlobResponse>();
+  const [progress, setProgress] = React.useState<number>();
+  const [received, setReceived] = React.useState<number>();
+  const [expected, setExpected] = React.useState<number>();
 
   React.useEffect(() => {
     const requestPermissionAndDownloadBlobAsync = async () => {
@@ -22,40 +33,56 @@ export const App = () => {
       } catch (err) {
         console.warn(err);
       }
-
-      const fetchedBlob = await BlobCourier.fetchBlob({
-        filename: '5MB.zip',
-        method: 'GET',
-        useDownloadManager: false,
-        url: 'http://ipv4.download.thinkbroadband.com/5MB.zip',
-      } as AndroidBlobRequest).onProgress((e: any) => {
-        console.debug('Recv download progress:', JSON.stringify(e));
-      });
-      const filePath =
-        fetchedBlob.type === BlobResponseType.Managed
-          ? (fetchedBlob.data as BlobManagedData).fullFilePath
-          : (fetchedBlob.data as BlobUnmanagedData).fullFilePath;
-
-      console.log(JSON.stringify(fetchedBlob), filePath);
-      setDownloadResult(fetchedBlob);
-
-      const uploadResult = await BlobCourier.uploadBlob({
-        filePath,
-        method: 'POST',
-        mimeType: 'text/plain',
-        url: 'https://file.io',
-      } as BlobUploadRequest).onProgress((e: any) => {
-        console.debug('Recv upload progress:', JSON.stringify(e));
-      });
-
-      console.warn(JSON.stringify(uploadResult));
     };
 
     requestPermissionAndDownloadBlobAsync();
   }, []);
 
+  const startDownload = async () => {
+    const fetchedBlob = await BlobCourier.fetchBlob({
+      filename: '5MB.zip',
+      method: 'GET',
+      useDownloadManager: true,
+      url: 'http://ipv4.download.thinkbroadband.com/5MB.zip',
+    } as AndroidBlobRequest).onProgress((e: any) => {
+      setProgress(parseInt(e.written, 10) / 5242880);
+      setReceived(parseInt(e.written, 10));
+      setExpected(parseInt(e.total, 10));
+    });
+
+    setDownloadResult(fetchedBlob);
+  };
+
+  const startUpload = async () => {
+    if (!downloadResult) {
+      return;
+    }
+
+    const filePath =
+      downloadResult.type === BlobResponseType.Managed
+        ? (downloadResult.data as BlobManagedData).fullFilePath
+        : (downloadResult.data as BlobUnmanagedData).fullFilePath;
+
+    const uploadResult = await BlobCourier.uploadBlob({
+      filePath,
+      method: 'POST',
+      mimeType: 'text/plain',
+      url: 'https://file.io',
+    } as BlobUploadRequest).onProgress((e: any) => {
+      console.log(JSON.stringify(e));
+    });
+
+    console.warn(JSON.stringify(uploadResult));
+  };
+
   return (
     <View style={styles.container}>
+      <Progress.Bar progress={progress} width={200} />
+      <Text>
+        {received}/{expected ?? '?'}
+      </Text>
+      <Button title="Download" onPress={() => startDownload()} />
+      <Button title="Upload" onPress={() => startUpload()} />
       <Text>Result: {JSON.stringify(downloadResult)}</Text>
     </View>
   );
