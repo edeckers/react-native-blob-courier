fa39e456a69bc912ee7427aa37f968abbad7e21d
Ely Deckers <e.deckers@gmail.com>
2020-12-21 15:58:01 +0100

    improvement: move download logic to class

--- android/src/main/java/io/deckers/blob_courier/BlobCourierModule.kt
+++ android/src/main/java/io/deckers/blob_courier/BlobCourierModule.kt
@@ -6,9 +6,6 @@
  */
 package io.deckers.blob_courier
 
-import android.app.DownloadManager
-import android.content.Context
-import android.content.IntentFilter
 import android.net.Uri
 import com.facebook.common.internal.ImmutableMap
 import com.facebook.react.bridge.Promise
@@ -17,28 +14,13 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.ReadableMap
 import com.facebook.react.modules.network.OkHttpClientProvider
-import java.io.File
 import java.lang.reflect.Type
 import java.net.URL
 import java.net.UnknownHostException
 import java.util.Locale
 import kotlin.concurrent.thread
-import okhttp3.Interceptor
-import okhttp3.Request
-import okhttp3.Response
-import okio.Okio
-import okio.Source
 
 private const val ERROR_MISSING_REQUIRED_PARAMETER = "ERROR_MISSING_REQUIRED_PARAMETER"
-
-private const val DOWNLOAD_MANAGER_PARAMETER_DESCRIPTION = "description"
-private const val DOWNLOAD_MANAGER_PARAMETER_ENABLE_NOTIFICATIONS = "enableNotifications"
-private const val DOWNLOAD_MANAGER_PARAMETER_TITLE = "title"
-
-private enum class TargetDirectoryEnum {
-  Cache,
-  Data
-}
 
 private val REQUIRED_PARAMETER_PROCESSORS = ImmutableMap.of(
   Boolean::class.java.toString(),
@@ -93,29 +75,6 @@ private fun assertRequiredParameter(input: ReadableMap, type: Type, parameterNam
     ERROR_MISSING_REQUIRED_PARAMETER,
     "`$parameterName` is a required parameter of type `$type`"
   )
-}
-
-private fun createDownloadProgressInterceptor(
-  reactContext: ReactApplicationContext,
-  taskId: String,
-  progressInterval: Int
-): (
-  Interceptor.Chain
-) -> Response = fun(
-  chain: Interceptor.Chain
-): Response {
-  val originalResponse = chain.proceed(chain.request())
-
-  return originalResponse.body()?.let {
-    originalResponse.newBuilder().body(
-      BlobCourierProgressResponse(
-        reactContext,
-        taskId,
-        progressInterval,
-        it
-      )
-    ).build()
-  } ?: originalResponse
 }
 
 private fun verifyFilePart(part: ReadableMap, promise: Promise): Boolean {
@@ -191,21 +150,7 @@ private fun filterHeaders(unfilteredHeaders: Map<String, Any>): Map<String, Stri
 class BlobCourierModule(private val reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
 
-  private val defaultDownloadManager =
-    reactContext.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-
   override fun getName(): String = LIBRARY_NAME
-
-  private fun createAbsoluteFilePath(
-    filename: String,
-    targetDirectory: TargetDirectoryEnum
-  ) =
-    File(
-      reactContext.let {
-        if (targetDirectory == TargetDirectoryEnum.Data) it.filesDir else it.cacheDir
-      },
-      filename
-    )
 
   private fun uploadBlobFromValidatedParameters(input: ReadableMap, promise: Promise) {
     val maybeTaskId = input.getString(PARAMETER_TASK_ID)
@@ -264,120 +209,6 @@ class BlobCourierModule(private val reactContext: ReactApplicationContext) :
     }
   }
 
-  private fun fetchBlobUsingDownloadManager(
-    taskId: String,
-    downloadManagerSettings: Map<String, Any>,
-    uri: Uri,
-    targetDirectory: TargetDirectoryEnum,
-    filename: String,
-    headers: Map<String, String>,
-    mimeType: String,
-    progressInterval: Int,
-    promise: Promise
-  ) {
-    val absoluteFilePath = createAbsoluteFilePath(filename, targetDirectory)
-
-    val request =
-      DownloadManager.Request(uri)
-        .setAllowedOverRoaming(true)
-        .setMimeType(mimeType)
-        .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-
-    if (downloadManagerSettings.containsKey(DOWNLOAD_MANAGER_PARAMETER_DESCRIPTION)) {
-      request.setDescription(
-        downloadManagerSettings[DOWNLOAD_MANAGER_PARAMETER_DESCRIPTION] as String
-      )
-    }
-
-    if (downloadManagerSettings.containsKey(DOWNLOAD_MANAGER_PARAMETER_TITLE)) {
-      request.setTitle(
-        downloadManagerSettings[DOWNLOAD_MANAGER_PARAMETER_TITLE] as String
-      )
-    }
-
-    val enableNotifications =
-      downloadManagerSettings.containsKey(DOWNLOAD_MANAGER_PARAMETER_ENABLE_NOTIFICATIONS) &&
-        downloadManagerSettings[DOWNLOAD_MANAGER_PARAMETER_ENABLE_NOTIFICATIONS] == true
-
-    request.setNotificationVisibility(if (enableNotifications) 1 else 0)
-
-    val downloadId = request
-      .let { requestBuilder
-        ->
-        requestBuilder.apply {
-          headers.forEach { e: Map.Entry<String, String> ->
-            addRequestHeader(e.key, e.value)
-          }
-        }
-
-        defaultDownloadManager.enqueue(
-          requestBuilder
-        )
-      }
-
-    val progressUpdater =
-      ManagedProgressUpdater(reactContext, taskId, downloadId, progressInterval.toLong())
-
-    progressUpdater.start()
-
-    reactContext.registerReceiver(
-      ManagedDownloadReceiver(downloadId, absoluteFilePath, progressUpdater, promise),
-      IntentFilter(
-        DownloadManager.ACTION_DOWNLOAD_COMPLETE,
-      )
-    )
-  }
-
-  private fun fetchBlobWithoutDownloadManager(
-    taskId: String,
-    uri: Uri,
-    targetDirectory: TargetDirectoryEnum,
-    filename: String,
-    headers: Map<String, String>,
-    method: String,
-    progressInterval: Int,
-    promise: Promise
-  ) {
-    val absoluteFilePath = createAbsoluteFilePath(filename, targetDirectory)
-
-    val request = Request.Builder()
-      .method(method, null)
-      .url(uri.toString())
-      .apply {
-        headers.forEach { e: Map.Entry<String, String> ->
-          addHeader(e.key, e.value)
-        }
-      }
-      .build()
-
-    val httpClient =
-      createHttpClient().newBuilder()
-        .addInterceptor(createDownloadProgressInterceptor(reactContext, taskId, progressInterval))
-        .build()
-
-    httpClient.newCall(request).execute().use { response ->
-      response.body()?.source().use { source ->
-        Okio.buffer(Okio.sink(absoluteFilePath)).use { sink ->
-
-          sink.writeAll(source as Source)
-        }
-      }
-
-      promise.resolve(
-        mapOf(
-          "type" to DOWNLOAD_TYPE_UNMANAGED,
-          "data" to mapOf(
-            "absoluteFilePath" to Uri.fromFile(absoluteFilePath),
-            "response" to mapOf(
-              "code" to response.code(),
-              "headers" to mapHeadersToMap(response.headers())
-            )
-          )
-        ).toReactMap()
-      )
-    }
-  }
-
   @Suppress("SameParameterValue")
   private fun getMapInt(input: ReadableMap, field: String, fallback: Int): Int =
     if (input.hasKey(field)) input.getInt(field) else fallback
@@ -394,11 +225,11 @@ class BlobCourierModule(private val reactContext: ReactApplicationContext) :
 
     val targetDirectoryOrFallback = (
       maybeAndroidSettings?.getString(PARAMETER_TARGET)
-        ?: TargetDirectoryEnum.Cache.toString()
+        ?: BlobDownloader.TargetDirectoryEnum.Cache.toString()
       )
 
     val maybeTargetDirectory =
-      TargetDirectoryEnum
+      BlobDownloader.TargetDirectoryEnum
         .values()
         .firstOrNull {
           it.name.toLowerCase(Locale.getDefault()) ==
@@ -442,59 +273,24 @@ class BlobCourierModule(private val reactContext: ReactApplicationContext) :
     }
 
     try {
-      startBlobFetch(
-        maybeTaskId,
-        Uri.parse(maybeUrl),
-        useDownloadManager,
-        downloadManagerSettings,
-        maybeTargetDirectory,
-        maybeFilename,
-        mimeType,
-        promise,
-        method,
-        headers,
-        progressInterval
-      )
+      BlobDownloader(reactContext, createHttpClient())
+        .startBlobFetch(
+          maybeTaskId,
+          Uri.parse(maybeUrl),
+          useDownloadManager,
+          downloadManagerSettings,
+          maybeTargetDirectory,
+          maybeFilename,
+          mimeType,
+          promise,
+          method,
+          headers,
+          progressInterval
+        )
     } catch (e: UnknownHostException) {
       promise.reject(ERROR_UNKNOWN_HOST, e)
     }
   }
-
-  private fun startBlobFetch(
-    taskId: String,
-    uri: Uri,
-    useDownloadManager: Boolean,
-    downloadManagerSettings: Map<String, Any>,
-    targetDirectory: TargetDirectoryEnum,
-    filename: String,
-    mimeType: String,
-    promise: Promise,
-    method: String,
-    headers: Map<String, String>,
-    progressInterval: Int
-  ) =
-    if (useDownloadManager)
-      fetchBlobUsingDownloadManager(
-        taskId,
-        downloadManagerSettings,
-        uri,
-        targetDirectory,
-        filename,
-        headers,
-        mimeType,
-        progressInterval,
-        promise
-      )
-    else fetchBlobWithoutDownloadManager(
-      taskId,
-      uri,
-      targetDirectory,
-      filename,
-      headers,
-      method,
-      progressInterval,
-      promise
-    )
 
   @ReactMethod
   fun fetchBlob(input: ReadableMap, promise: Promise) {
---
+++ android/src/main/java/io/deckers/blob_courier/BlobDownloader.kt
@@ -0,0 +1,217 @@
+package io.deckers.blob_courier
+
+import android.app.DownloadManager
+import android.content.Context
+import android.content.IntentFilter
+import android.net.Uri
+import com.facebook.react.bridge.Promise
+import com.facebook.react.bridge.ReactApplicationContext
+import java.io.File
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import okio.Okio
+import okio.Source
+
+private const val DOWNLOAD_MANAGER_PARAMETER_DESCRIPTION = "description"
+private const val DOWNLOAD_MANAGER_PARAMETER_ENABLE_NOTIFICATIONS = "enableNotifications"
+private const val DOWNLOAD_MANAGER_PARAMETER_TITLE = "title"
+
+private fun createDownloadProgressInterceptor(
+  reactContext: ReactApplicationContext,
+  taskId: String,
+  progressInterval: Int
+): (
+  Interceptor.Chain
+) -> Response = fun(
+  chain: Interceptor.Chain
+): Response {
+  val originalResponse = chain.proceed(chain.request())
+
+  return originalResponse.body()?.let {
+    originalResponse.newBuilder().body(
+      BlobCourierProgressResponse(
+        reactContext,
+        taskId,
+        progressInterval,
+        it
+      )
+    ).build()
+  } ?: originalResponse
+}
+
+class BlobDownloader(
+  private val reactContext: ReactApplicationContext,
+  private val httpClient: OkHttpClient
+) {
+
+  enum class TargetDirectoryEnum {
+    Cache,
+    Data
+  }
+
+  private val defaultDownloadManager =
+    reactContext.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+
+  private fun createAbsoluteFilePath(
+    filename: String,
+    targetDirectory: TargetDirectoryEnum
+  ) =
+    File(
+      reactContext.let {
+        if (targetDirectory == TargetDirectoryEnum.Data) it.filesDir else it.cacheDir
+      },
+      filename
+    )
+
+  fun startBlobFetch(
+    taskId: String,
+    uri: Uri,
+    useDownloadManager: Boolean,
+    downloadManagerSettings: Map<String, Any>,
+    targetDirectory: TargetDirectoryEnum,
+    filename: String,
+    mimeType: String,
+    promise: Promise,
+    method: String,
+    headers: Map<String, String>,
+    progressInterval: Int
+  ) =
+    if (useDownloadManager)
+      fetchBlobUsingDownloadManager(
+        taskId,
+        downloadManagerSettings,
+        uri,
+        targetDirectory,
+        filename,
+        headers,
+        mimeType,
+        progressInterval,
+        promise
+      )
+    else fetchBlobWithoutDownloadManager(
+      taskId,
+      uri,
+      targetDirectory,
+      filename,
+      headers,
+      method,
+      progressInterval,
+      promise
+    )
+
+  private fun fetchBlobUsingDownloadManager(
+    taskId: String,
+    downloadManagerSettings: Map<String, Any>,
+    uri: Uri,
+    targetDirectory: TargetDirectoryEnum,
+    filename: String,
+    headers: Map<String, String>,
+    mimeType: String,
+    progressInterval: Int,
+    promise: Promise
+  ) {
+    val absoluteFilePath = createAbsoluteFilePath(filename, targetDirectory)
+
+    val request =
+      DownloadManager.Request(uri)
+        .setAllowedOverRoaming(true)
+        .setMimeType(mimeType)
+        .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+
+    if (downloadManagerSettings.containsKey(DOWNLOAD_MANAGER_PARAMETER_DESCRIPTION)) {
+      request.setDescription(
+        downloadManagerSettings[DOWNLOAD_MANAGER_PARAMETER_DESCRIPTION] as String
+      )
+    }
+
+    if (downloadManagerSettings.containsKey(DOWNLOAD_MANAGER_PARAMETER_TITLE)) {
+      request.setTitle(
+        downloadManagerSettings[DOWNLOAD_MANAGER_PARAMETER_TITLE] as String
+      )
+    }
+
+    val enableNotifications =
+      downloadManagerSettings.containsKey(DOWNLOAD_MANAGER_PARAMETER_ENABLE_NOTIFICATIONS) &&
+        downloadManagerSettings[DOWNLOAD_MANAGER_PARAMETER_ENABLE_NOTIFICATIONS] == true
+
+    request.setNotificationVisibility(if (enableNotifications) 1 else 0)
+
+    val downloadId = request
+      .let { requestBuilder
+        ->
+        requestBuilder.apply {
+          headers.forEach { e: Map.Entry<String, String> ->
+            addRequestHeader(e.key, e.value)
+          }
+        }
+
+        defaultDownloadManager.enqueue(
+          requestBuilder
+        )
+      }
+
+    val progressUpdater =
+      ManagedProgressUpdater(reactContext, taskId, downloadId, progressInterval.toLong())
+
+    progressUpdater.start()
+
+    reactContext.registerReceiver(
+      ManagedDownloadReceiver(downloadId, absoluteFilePath, progressUpdater, promise),
+      IntentFilter(
+        DownloadManager.ACTION_DOWNLOAD_COMPLETE,
+      )
+    )
+  }
+
+  private fun fetchBlobWithoutDownloadManager(
+    taskId: String,
+    uri: Uri,
+    targetDirectory: TargetDirectoryEnum,
+    filename: String,
+    headers: Map<String, String>,
+    method: String,
+    progressInterval: Int,
+    promise: Promise
+  ) {
+    val absoluteFilePath = createAbsoluteFilePath(filename, targetDirectory)
+
+    val request = Request.Builder()
+      .method(method, null)
+      .url(uri.toString())
+      .apply {
+        headers.forEach { e: Map.Entry<String, String> ->
+          addHeader(e.key, e.value)
+        }
+      }
+      .build()
+
+    val httpClient =
+      httpClient.newBuilder()
+        .addInterceptor(createDownloadProgressInterceptor(reactContext, taskId, progressInterval))
+        .build()
+
+    httpClient.newCall(request).execute().use { response ->
+      response.body()?.source().use { source ->
+        Okio.buffer(Okio.sink(absoluteFilePath)).use { sink ->
+
+          sink.writeAll(source as Source)
+        }
+      }
+
+      promise.resolve(
+        mapOf(
+          "type" to DOWNLOAD_TYPE_UNMANAGED,
+          "data" to mapOf(
+            "absoluteFilePath" to Uri.fromFile(absoluteFilePath),
+            "response" to mapOf(
+              "code" to response.code(),
+              "headers" to mapHeadersToMap(response.headers())
+            )
+          )
+        ).toReactMap()
+      )
+    }
+  }
+}
