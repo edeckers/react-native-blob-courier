ba65b1ef3aff7d0ec31f9b41fb7481f6bca25024
Ely Deckers <e.deckers@gmail.com>
2021-01-23 17:09:55 +0100

    refactor: add React imports

--- ios/Fetch/BlobDownloader.swift
+++ ios/Fetch/BlobDownloader.swift
@@ -3,7 +3,9 @@
 //  This source code is licensed under the MPL-2.0 license found in the
 //  LICENSE file in the root directory of this source tree.
 import Foundation
+import React
 
+@objc(BlobDownloader)
 open class BlobDownloader: NSObject {
   static func filterHeaders(unfilteredHeaders: NSDictionary) -> NSDictionary {
     Dictionary(uniqueKeysWithValues: unfilteredHeaders
--- ios/Progress/BlobCourierEventEmitter.swift
+++ ios/Progress/BlobCourierEventEmitter.swift
@@ -3,6 +3,7 @@
 // This source code is licensed under the MPL-2.0 license found in the
 // LICENSE file in the root directory of this source tree.
 import Foundation
+import React
 
 @objc(BlobCourierEventEmitter)
 open class BlobCourierEventEmitter: RCTEventEmitter {
