8599af3a3bb930024b82e1fff4155cad7fb5740c
Ely Deckers <e.deckers@gmail.com>
2021-01-23 21:05:12 +0100

    fix: export all relevant methods

--- ios/BlobCourierEventEmitter.swift
+++ ios/BlobCourierEventEmitter.swift
@@ -4,6 +4,7 @@
 // LICENSE file in the root directory of this source tree.
 import Foundation
 
+@objc(BlobCourierEventEmitter)
 open class BlobCourierEventEmitter: RCTEventEmitter {
   public static let eventProgress = "BlobCourierProgress"
 
